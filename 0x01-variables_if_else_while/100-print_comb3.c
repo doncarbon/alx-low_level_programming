7b170cc9a3798a37d7a9915d672a24702116982a
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-06 16:43:39 -0700

    100-print_comb3.c task fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 38; n <= 46; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (m = 39; m <= 47; m++)
+		for (m = 49; m <= 57; m++)
 		{
 			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if (n != 46 || m != 47)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
