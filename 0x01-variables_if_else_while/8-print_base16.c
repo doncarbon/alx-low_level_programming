240e7ff2157ddf4a43c5e0c0d78fa489b747dd2d
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-06 15:46:33 -0700

    Task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (c = a; c <= 'f'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
