aec0c42f64f2714527d075ce3df27216caa13aa8
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-10 13:51:29 -0700

    1-alphabet.c fixes + task 3 done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,17 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
+void print_alphabet_x10(void);
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char a;
+int i;
+for (i = 0; i < 10; i++)
+{
 for (a = 'a'; a <= 'z'; a++)
 {
 printf("%c", a);
 }
-printf("\n");
+}
 }
