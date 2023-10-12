52f54dea1ba8c290744b5fde4038ad1af01e4108
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-12 12:40:21 -0700

    Hot FIX

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ int i;
 
 for (i = 0; i < 10; i++)
 {
-if (i != 2 || i != 4)
+if (i != 2 && i != 4)
 {
  _putchar(i + '0');
 }
 }
-
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers from 0 to 14
- * followed by a new line.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 void more_numbers(void)
 {
-int i;
-int t;
+int i, j;
 
-for (t = 0; t < 10; t++)
+for (i = 0; i < 10; i++)
 {
-for (i = 0; i < 15; i++)
+for (j = 0; j < 15; j++)
 {
-_putchar(i + '0');
+if (j >= 10)
+{
+_putchar(j / 10 + '0');
+}
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
