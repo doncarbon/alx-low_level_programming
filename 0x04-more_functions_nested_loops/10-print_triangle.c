838bb88a8380a875724870eba605e89f8d5dd115
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-12 14:07:55 -0700

    Error fixes

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,6 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
- 
 void print_triangle(int size)
 {
 if (size <= 0)
@@ -21,12 +20,10 @@ for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-
 for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
-
 _putchar('\n');
 }
 }
