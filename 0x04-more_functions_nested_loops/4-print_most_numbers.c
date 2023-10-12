b44e152a740a825a060bf1f16e326f974111494d
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-12 12:36:48 -0700

    Task fixes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,9 +2,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9
  * followed by a new line.
- * Return: 0 always success
  */
 void print_most_numbers(void)
 {
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
+ * more_numbers - prints 10 times the numbers from 0 to 14
  * followed by a new line.
- * Return: 0 always success
  */
 void more_numbers(void)
 {
-int i, t;
+int i;
+int t;
 
 for (t = 0; t < 10; t++)
 {
