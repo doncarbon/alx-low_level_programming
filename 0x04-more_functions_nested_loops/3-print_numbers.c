0dd833a68ff660427aa2a0585a53f83f9046bbac
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-12 12:15:30 -0700

    Deleted some trash files and fixed last task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- * Return: 0 always success
  */
 void print_numbers(void)
 {
@@ -13,5 +12,6 @@ for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 }
+
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h~
+++
@@ -1,6 +0,0 @@
-#ifndef main_h
-#define main_h
-
-int _isupper(int c);
-
-#endif
