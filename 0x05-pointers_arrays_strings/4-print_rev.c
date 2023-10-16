6bacb850f8bf2f1c6ac7c4550e981aad035a596e
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-16 12:28:45 -0700

    Task 4-5 fixes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@ void print_rev(char *s)
 	{
 		len++;
 	}
-	for (i = len - 1; i >= 0; i++)
+	for (i = len - 1; i >= 0; i--)
 	{
 		printf("%c", s[i]);
 	}
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,18 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len = 0;
+	int i, len = 0, len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
+	len1 = len - 1;
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[(len - 1)];
-		s[(len - 1) - 1] = tmp;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
