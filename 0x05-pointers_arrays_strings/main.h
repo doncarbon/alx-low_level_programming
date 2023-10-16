6d1ed64930261204adab52390c28cf01a28084c3
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-16 12:52:01 -0700

    task 7 done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
