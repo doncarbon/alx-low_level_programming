ffdd99089ea73b8303944838322ae5495f50f810
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-16 11:05:22 -0700

    Task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string provided
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i = 0, len;
+	char c = *s;
+
+	for (c = c[i] ; c != '\\0'; i++)
+	{
+		len = i + 1
+	}
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _strlen(char *s);
 void swap_int(int *a, int *b);
 int _putchar(char c);
 void reset_to_98(int *n);
