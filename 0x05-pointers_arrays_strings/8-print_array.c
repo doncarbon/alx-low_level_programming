aee7161b151f91b55d43f13367ffaf1a76488b21
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-16 13:02:01 -0700

    Fixed betty errors

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * puts_half - prints n elements of an array of integers
+ * print_array - prints n elements of an array of integers
  * followed by a new line.
  * @a: a provided
  * @n: number provided
@@ -11,6 +11,7 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
