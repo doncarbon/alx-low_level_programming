c5ee9a81519cef99b4925f8bdfb106e35366a37e
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-16 11:42:31 -0700

    Task fixes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,7 +8,8 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;	
+	int len = 0;
+	
 	while (*s != '\0')
 	{
 		len++;
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 /**
- * _strlen - prints a string, followed by a new line, to stdout.
+ * _puts - prints a string, followed by a new line, to stdout.
  * @str: string provided
  * Return: Always 0.
  */
  void _puts(char *str)
 {
-	while (*str != '\n')
+	int i = 0;
+
+	while (i < _strlen(str))
 	{
 		printf("%c", *str);
 		str++;
