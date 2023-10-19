148df14ef61a8e5bb789d937eaac141d9440a0fe
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-19 16:42:39 -0700

    Code update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,24 +5,18 @@
  * @s1: string 1
  * @s2: string 2
  *
- * Return: comparaison
+ * Return: comparison
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-int i = 0, j = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] > s2[i])
-{
-j++;
-}
-else if (s1[i] < s2[i])
-{
-j--;
-}
-i++;
-}
-
-return (j);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
