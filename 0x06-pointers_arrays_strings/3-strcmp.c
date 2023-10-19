e770e7c64347919e2755d796edbe99e3fede0ee7
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-19 16:38:07 -0700

    Betty fixes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,31 +2,27 @@
 
 /**
  * _strcmp - compares two strings.
- * @s1: string 1 
- * @s2: string 2 
+ * @s1: string 1
+ * @s2: string 2
  *
  * Return: comparaison
  */
 int _strcmp(char *s1, char *s2);
 {
-    int i = 0, j = 0;
-    
-    while (s1[i] != '\0' && s2[i] != '\0')
-    {
-        if (s1[i] > s2[i])
-        {
-            j++;
-        }
-        else if (s1[i] = s2[i])
-        {
-            j = j;
-        }
-        else (s1[i] > s2[i])
-        {
-            j--;;
-        }
-        i++;
-    }
-    
-    return (j);
+int i = 0, j = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] > s2[i])
+{
+j++;
+}
+else if (s1[i] > s2[i])
+{
+j--;
+}
+i++;
+}
+
+return (j);
 }
