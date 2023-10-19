205218cda793ae97a8bbf19b9e9d99c6d9a246b4
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-19 16:31:22 -0700

    Task 3 done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcpy(char *dest, char *src);
