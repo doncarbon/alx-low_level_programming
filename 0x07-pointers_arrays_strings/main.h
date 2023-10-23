258c2c59733ae4832516715f225eb0f89da22e4f
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-23 09:37:56 -0700

    Header file created, README.md created

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef main_h
+#define main_h
+
+void print_number(int n);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+int _atoi(char *s);
+void puts_half(char *str);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+void reset_to_98(int *n);
+char *_strcat(char *dest, char *src);
+
+#endif
