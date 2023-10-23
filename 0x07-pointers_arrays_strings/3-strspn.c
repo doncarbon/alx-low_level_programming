60bc7540207e6792ecd46e63b2ca8c0ad58d28f9
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-23 12:59:26 -0700

    Task 3 done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: memory area to be checked
+ * @accept: char to check
+ *
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = 0, is = 0, i, j;
+	unsigned int counter = 0;
+
+	while (accept[len] != '\n')
+	{
+		len++;
+	}
+
+	while (s[is] != ' ')
+	{
+		is++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < is; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				counter++;
+			}
+		}
+	}
+
+	return (counter);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
