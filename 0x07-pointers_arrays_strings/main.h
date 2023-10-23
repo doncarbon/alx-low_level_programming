b4e589e965e4eaff8f93deb20003b70c3f4789ef
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-23 14:12:56 -0700

    Task 5 done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
