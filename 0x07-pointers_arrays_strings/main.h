27aca4b86599abd28d45dd4ef33eaa12baede883
Hamza Yassine <ihamzayassine@gmail.com>
2023-10-24 10:42:58 -0700

    Task 8 done

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
