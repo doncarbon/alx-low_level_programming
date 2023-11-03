9737a007994235d0fcd1d3459b9dcde18ca9e6f7
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-02 17:07:46 -0700

    Fixed code and added memset function to the code and the prototype to the header file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,26 @@
 #include <limits.h>
 
 /**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
  * _calloc - allocates memory for an array, using malloc.
  * @nmemb: number of elements
  * @size: size of the bytes
@@ -12,16 +32,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void **pt;
+	void *pt;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	*pt = malloc(nmemb);
-	pt = malloc(size);
+	pt = malloc(size * nmemb);
 
-	if (!*pt)
+	if (pt == NULL)
 		return (NULL);
 
-	return (*pt);
+	_memset(pt, 0, nmemb * size);
+
+	return (pt);
 }
