0356811e0b2ed9c75858e77947a33b1f4d765c4c
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-06 12:21:16 -0800

    Task 4 fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,12 +52,12 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * new_dog - creates a new dog.
- * @name: name to initialize
- * @age: age to initialize
- * @owner: owner to initialize
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * return: a pointer to the new functioni if success, NULL if failed
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
