a256f449959c412aceaa39a85b878bdafa4aa0a5
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-06 12:18:51 -0800

    Task 3 done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -56,7 +56,35 @@ char *_strcpy(char *dest, char *src)
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
+ *
+ * return: a pointer to the new functioni if success, NULL if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 }
