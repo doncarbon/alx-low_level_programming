99c42f7fdb34c08908f0633fd32d20fa2ad29500
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-06 09:41:17 -0800

    Task 2 fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name == "(nil)";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner == "(nil)";
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner:%s\n", d->name, d->age, d->owner);
 }
