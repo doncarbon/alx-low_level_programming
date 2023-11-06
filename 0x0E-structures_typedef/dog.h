211bb21f204c6806e1a725ccb2e5d1fe0be3eef8
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-06 12:31:59 -0800

    Task 5 done'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,7 @@ struct dog
  */
 typedef struct dog dog_t;
 
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
