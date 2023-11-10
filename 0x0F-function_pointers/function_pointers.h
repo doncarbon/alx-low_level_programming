848e5a49b88fc4d70514150f23283fd53cb65f17
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-09 16:40:40 -0800

    Task 2 done for function pointers project

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
