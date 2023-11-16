567e441bd54daa15ff9763f0b0c61b41fa322d7b
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-16 03:02:37 -0800

    First task draft

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: head of the lists
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
