0afebf6ebdc3a4a3c73a6a57df27d4917858a081
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-16 03:21:55 -0800

    Task 0 update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,4 +8,20 @@
  */
 size_t print_list(const list_t *h)
 {
+	list_t *element = h;
+	unsigned int i, nodes = 0;
+
+	if (element == NULL)
+		printf("[0] (nil)");
+
+	while (element != NULL)
+	{
+		printf("[%d] ", element->len);
+		for (i = 0; i < element.len; i++)
+			 printf("%s", element->str[i]);
+		printf("\n");
+		element = element->next;
+		nodes++;
+	}
+	return (nodes);
 }
