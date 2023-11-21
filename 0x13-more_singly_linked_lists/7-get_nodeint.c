b3895b425a24cd5d01977aa6fab553859dc6bec3
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-21 14:13:07 -0800

    Task 7 done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count = 1;
+	unsigned int count = 0;
 	listint_t *node = NULL;
 
 	while (head && count != index)
 	{
-		node = head;
-		head = head->next;
 		count++;
+		head = head->next;
+		node = head;
 	}
 
 	if (node == NULL)
