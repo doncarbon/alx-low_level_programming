94cc2c1bdd78510e451a7fdd22ff9c1bca6c399b
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-20 18:17:34 -0800

    Task 5 done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,11 @@ void free_listint2(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head)
+	while (*head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
