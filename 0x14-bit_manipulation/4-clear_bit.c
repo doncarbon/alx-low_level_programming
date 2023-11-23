e7cce5d984014d93e9cb7ae31a7e96a30c1a3f70
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-23 15:51:09 -0800

    Task 4 fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n &= (n << index) && 0;
+	*n &= ~(1 << index);
 
 	return (1);
 }
