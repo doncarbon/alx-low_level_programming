5b64a5857a7a78181449cd39539c9cb832ccbc5c
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-23 15:45:27 -0800

    Betty fixes for task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @n: address of the number
  * @index: given index
  *
- * Returns: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
