744b0c456b719f6ad6de20765c8ef489cf8ad57f
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-27 03:37:18 -0800

    Fixed Task 1 output file permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	int o;
+	ssize_t w;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = fopen(filename, "w+");
-	if (file == NULL)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (text_content != NULL)
-		fprintf(file, "%s", text_content);
-
-	fclose(file);
+	close(o);
 
 	return (1);
 }
