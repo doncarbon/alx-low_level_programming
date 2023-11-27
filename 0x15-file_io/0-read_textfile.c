7939d5f48a27b98d0a3e0e25c12e3f7f804272cd
Hamza Yassine <ihamzayassine@gmail.com>
2023-11-27 01:17:27 -0800

    Task 0 draft

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters
+ *
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fileread, filewrite, fileopen;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fileopen = open(filename, 0_RDONLY);
+	fileread = read(fileopen, buffer, letters);
+	filewrite = write(STDOUT_FILENO, buffer, fileread);
+
+	if (filewrite == -1 || fileopen == -1 || filewrite == -1 || filewrite != fileread)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fileopen);
+
+	return (filewrite);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
