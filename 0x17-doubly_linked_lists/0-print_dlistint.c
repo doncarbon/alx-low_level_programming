d78f0f69c0ffdd8a8fbe96efa308fddeb9093a5f
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-09 14:29:23 -0800

    Task 0 done

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h)
+	{
+		count += 1;
+		printf("%d", h->n);
+		h = h->next;
+	}
+
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,7 @@
+# 0x17. C - Doubly linked lists
+
+Welcome to the 0x17. C - Doubly linked lists project! This project is part of the ALX Software Engineering curriculum.
+
+## About the Author
+- **Name:** Hamza YASSINE
+- **ALX Software Engineering Student** 
