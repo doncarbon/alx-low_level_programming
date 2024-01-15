6fa5e9513d026e89a20ba0415da5975e8beb2603
Hamza Yassine <ihamzayassine@gmail.com>
2024-01-14 22:32:45 -0800

    Task 2 done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+ * add - addition of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result of a - b.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication of 2 number
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result of a / b.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - module of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
