d305823694120b7da08d3063bed6cbe5633e47d5
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-02 02:31:54 -0800

    Task 3 update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = value_dup;
 			return (1);
 		}
 	}
