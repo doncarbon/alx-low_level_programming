064d468813781b7ac4dee1c9eb8f4d2f1a180bc6
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-02 02:57:50 -0800

    Task 5 done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i, index;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table to print.
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	unsigned char iscomma = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 1; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (iscomma = 1)
+				printf(", ");
+
+			while (ht->array[i] != NULL)
+			{
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				ht->array[i] = ht->array[i]->next;
+				if (ht->array[i] == NULL)
+					printf(", ");
+			}
+
+			iscomma = 1
+		}
+	}
+	printf("}\n");
+}
