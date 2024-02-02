d4fc15cc92e197ca9505123c5b531a5f266b5b40
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-02 02:07:37 -0800

    Task 2 done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
-	unsigned long int i = 0;
+	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
 
