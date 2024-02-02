6d0ae69d6a9b766ba538723ac26878d9285bad03
Hamza Yassine <ihamzayassine@gmail.com>
2024-02-02 03:24:28 -0800

    TAsk 6 dupdate

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -19,12 +19,14 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		if (ht->array[i] != NULL)
 		{
-			while (tmp != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				tmp = ht->array[i]->next;
-				free(ht->array[i]->key);
-				free(ht->array[i]->value);
-				free(ht->array[i]);
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
 	}
